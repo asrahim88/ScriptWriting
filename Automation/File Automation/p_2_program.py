0c0e63e06922f119431d675e45aa608405e9f667
asrahim88 <asrahim162@gmail.com>
2025-06-17 12:52:48 +0600

    somethig updated

--- Automation/File Automation/p_2_program.py
+++ Automation/File Automation/p_2_program.py
@@ -4,5 +4,19 @@ with open("note.txt", "w") as file:
     file.write("How are you all programmers\n")
     file.write("I Love python scripting\n")
 
+
 a = p_1_program.customFile("all")
 print(a)
+
+lines = [
+    "line1 : I play football\n",
+    "line2 : I play cricket\n",
+    "line3 : I play chess\n",
+]
+with open("note.txt", "a") as file:
+    file.write("Hi i am new line newly adding.\n")
+    file.writelines(lines)
+
+a = p_1_program.customFile("lines")
+for i in a:
+    print(i, end=" ")
